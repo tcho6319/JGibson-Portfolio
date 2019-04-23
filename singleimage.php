8c8bf8c9a21e1feefd4a1545071dfd4398e8d3fc
Jinny Lee <jl3282@cornell.edu>
2019-04-22 22:26:28 -0400

    general codes

---
+++ singleimage.php
@@ -0,0 +1,17 @@
+<?php
+include("includes/init.php");
+?>
+<!DOCTYPE html>
+<html lang="en">
+
+<?php include("includes/head.php");?>
+
+
+
+<body>
+  <?php include("includes/header.php");?>
+
+
+  <?php include("includes/footer.php");?>
+</body>
+</html>
